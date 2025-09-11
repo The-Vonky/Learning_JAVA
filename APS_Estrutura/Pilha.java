19c2dcecf9a067b1e14f5da73fef1c3f0b7f8fd5
Deywid Braga <deywidbraga14@gmail.com>
2025-09-11 01:07:17 +0000

    Codigos atualizados

---
+++ APS_Estrutura/Pilha.java
@@ -0,0 +1,14 @@
+class Pilha {
+    public static void executar(){
+        // Criando a pilha do sandubão
+        Stack<String> pilhaSanduba = new Stack<>();
+
+        // 
+        pilhaSanduba.push("Base do Pão")
+        pilhaSanduba.push("Alface")
+        pilhaSanduba.push("Tomate")
+        pilhaSanduba.push("Hamburguer")
+        pilhaSanduba.push("Alface")
+        pilhaSanduba.push("Pão em cima")
+    }
+}
